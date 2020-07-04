df8d706cff7c89e51fdce2cd7157b9ea89659ca5
unknown <cheahwen1997@gmail.com>
2020-07-04 22:09:56 +0800

    Implement Attendance Schema

--- server/models/attendance.model.js
+++ server/models/attendance.model.js
@@ -1,34 +1,37 @@
-const {model, Schema}=require('mongoose')
+const { model, Schema } = require("mongoose");
 
-const attendanceSchema=new Schema({
-    course:{
-        type: Schema.Types.ObjectId,
-        required: true
+const attendanceSchema = new Schema(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      required: true,
     },
-    start:{
-        type: String,
-        required: true
+    start: {
+      type: String,
+      required: true,
     },
-    end:{
-        type: String,
-        required: true
+    end: {
+      type: String,
+      required: true,
     },
-    date:{
-        type: String,
-        required: true
+    date: {
+      type: String,
+      required: true,
     },
     gPhoto: {
-        type: [Schema.Types.ObjectId],
-        ref: 'GroupPhoto'
+      type: [Schema.Types.ObjectId],
+      ref: "GroupPhoto",
     },
     attendees: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Person'
+      type: [Schema.Types.ObjectId],
+      ref: "Person",
     },
     absentees: {
-        type: [Schema.Types.ObjectId],
-        ref: 'Person'
-    }
-})
+      type: [Schema.Types.ObjectId],
+      ref: "Person",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports=model('Attendance', attendanceSchema)
+module.exports = model("Attendance", attendanceSchema);
