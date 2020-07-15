44eb62237f19eed1aef17c910c57a5526638330c
unknown <cheahwen1997@gmail.com>
2020-07-15 13:22:55 +0800

    Add Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      This is our home
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route component={()=><div>No found</div>} />
+    </Router>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function SignIn() {
+export default function Home() {
     return (
         <div>
-            SignIn
+            Home
         </div>
     )
 }
